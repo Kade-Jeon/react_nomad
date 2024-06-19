f631a123942b54f73a23d2ff662f7be5f0d90aa6
kade <zsc0248@gmail.com>
2024-06-19 22:25:21 +0900

    Chapter3-6: minute converter to hour using useState

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-  var [counter, setCounter] = useState(0);
-  var countUp = () => {
-    console.log(counter);
-    setCounter((current) => current + 1);
-  };
-  return (
-    <div className="App">
-      <div>
-        <h3>Total Clicks: {counter}</h3>
-        <button onClick={countUp}>click me</button>
-      </div>
-    </div>
-  );
+    const [minutes, setMinutes] = useState();
+    const [hours, setHours] = useState(0);
+    const onChange = (event) => {
+        setMinutes(event.target.value);
+    };
+    const reset = () => {
+        setMinutes(0);
+    };
+
+    return (
+        <div className="App">
+            <h1>Super Converter</h1>
+            <div>
+                <label htmlFor="minutes">minutes</label>
+                <input id="minutes" placeholder="minutes" type="number" value={minutes} onChange={onChange}/>
+            </div>
+            <div>
+                <h4>You want to convert </h4>
+                <label htmlFor="hours">hours</label>
+                <input id="hours" placeholder="hour" type="number" value={minutes / 60}/>
+            </div>
+            <div>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
