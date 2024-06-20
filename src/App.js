544df587d92976f11c890a3f83e18b5e9d27fe06
kade <zsc0248@gmail.com>
2024-06-20 22:48:35 +0900

    Chapter3-9: Finish converter 1. Minute <-> Hour 2. Km <-> Mile

--- src/App.js
+++ src/App.js
@@ -2,31 +2,124 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
-function App() {
-    const [minutes, setMinutes] = useState();
-    const [hours, setHours] = useState(0);
+function MinuteAndHour() {
+    const [amount, setAmount] = useState(0);
+    const [inverted, setInverted] = useState(true);
     const onChange = (event) => {
-        setMinutes(event.target.value);
+        setAmount(event.target.value);
     };
     const reset = () => {
-        setMinutes(0);
+        setAmount(0);
+    };
+
+    const onInverted = () => {
+        setInverted(current => !current);
+        reset();
     };
 
     return (
-        <div className="App">
-            <h1>Super Converter</h1>
+        <div className="MinuteAndHour">
             <div>
                 <label htmlFor="minutes">minutes</label>
-                <input id="minutes" placeholder="minutes" type="number" value={minutes} onChange={onChange}/>
+                <input id="minutes" placeholder="minutes" type="number" value={inverted ? amount : amount * 60}
+                       onChange={onChange} disabled={!inverted}/>
             </div>
+            <h4>↕️</h4>
             <div>
-                <h4>You want to convert </h4>
                 <label htmlFor="hours">hours</label>
-                <input id="hours" placeholder="hour" type="number" value={minutes / 60}/>
+                <input id="hours" placeholder="hour" type="number" value={inverted ? amount / 60 : amount}
+                       onChange={onChange} disabled={inverted}/>
             </div>
             <div>
-                <button onClick={reset}>Reset</button>
+                <p>
+                    <button onClick={reset}>Reset</button>
+                    <button onClick={onInverted}>{inverted ? "Minute To Hour" : "Hour To Minute"}</button>
+                </p>
             </div>
+        </div>
+    );
+}
+
+function KmAndMile() {
+    const [distance, setDistance] = useState(0);
+    const [inverted, setInverted] = useState(false);
+    const reset = () => {
+        setDistance(0);
+    }
+
+    const onInverted = () => {
+        setInverted(current => !current);
+        reset();
+    };
+
+    const onChange = (event) => {
+        setDistance(event.target.value);
+    }
+
+    return (
+        <div className="KmToMile">
+            <div>
+                <label htmlFor="km">Km</label>
+                <input type="number" id="km" name="Km" onChange={onChange} value={inverted ? distance / 0.62 : distance}
+                       disabled={inverted}/>
+            </div>
+            <h4>↕️</h4>
+            <div>
+                <label htmlFor="mile">Mile</label>
+                <input type="number" id="mile" name="Mile" onChange={onChange}
+                       value={inverted ? distance : distance * 0.62} disabled={!inverted}/>
+            </div>
+            <div>
+                <p>
+                    <button onClick={reset}>Reset</button>
+                    <button onClick={onInverted}>{inverted ? "Mile To Km" : "Km To Mile"}</button>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function Select() {
+    const [choice, setChoice] = useState(-1);
+    const onSelect = (event) => {
+        setChoice(Number(event.target.value));
+    };
+
+    const options = [
+        {id: 1, value: -1, content: "Select Converting Options"},
+        {id: 2, value: 0, content: "Minute & Hour"},
+        {id: 3, value: 1, content: "Km & Mile"}
+    ];
+
+    const renderConverter = () => {
+        switch (choice) {
+            case 0:
+                return <MinuteAndHour />
+            case 1:
+                return <KmAndMile />
+            default:
+                return "Please Select Converting Options";
+        }
+    };
+
+    return (
+        <div className="Select">
+            <select value={choice} onChange={onSelect}>
+                {options.map(option => {
+                    return <option key={option.id} value={option.value}>{option.content}</option>
+                })}
+            </select>
+            <hr/>
+            {renderConverter()}
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Super Converter</h1>
+            <Select />
         </div>
     );
 }
